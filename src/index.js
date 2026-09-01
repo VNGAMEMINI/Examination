0e6b87a20f5c4c43407f16af6dc36c72bd38c7c7
VNGAMEMINI <pnsong2707@gmail.com>
2026-09-01 15:00:51 +0700

    aa

--- src/index.js
+++ src/index.js
@@ -3,29 +3,17 @@ import Examination from "./examination/Examination.js";
 export { default as Answer } from "./answer/Answer.js";
 export { default as Question } from "./question/Question.js";
 
-export { default as normalize } from "./normalize/normalize.js";
-export { default as normalizeAnswer } from "./normalize/normalizeAnswer.js";
-export { default as normalizeQuestion } from "./normalize/normalizeQuestion.js";
-
-export { default as validate } from "./validate/validate.js";
-export { default as validateAnswer } from "./validate/validateAnswer.js";
-export { default as validateQuestion } from "./validate/validateQuestion.js";
-
-export { default as compare } from "./compare/compare.js";
-export { default as compareAnswer } from "./compare/compareAnswer.js";
-
 export { default as Result } from "./result/Result.js";
-export {
-  default as evaluate,
-  evaluateCollection,
-} from "./evaluate/evaluate.js";
+export { default as Summary } from "./summary/Summary.js";
+export { default as Score } from "./score/Score.js";
 
 export { default as ValidationError } from "./errors/ValidationError.js";
 
-export { default as Summary } from "./summary/Summary.js";
+export { default as normalize } from "./normalize/normalize.js";
+export { default as validate } from "./validate/validate.js";
+export { default as compare } from "./compare/compare.js";
+export { default as evaluate } from "./evaluate/evaluate.js";
 export { default as summarize } from "./summary/summarize.js";
-
-export { default as Score } from "./score/Score.js";
 export { default as score } from "./score/score.js";
 
 export { Examination };
